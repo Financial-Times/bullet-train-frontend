9d754e8d59df28e388b574d9e450d388038c1d6b
ola.okejimi <ola.okejimi@ft.com>
2019-10-01 12:59:14 +0100

    add config files

--- bin/env.js
+++ bin/env.js
@@ -5,8 +5,8 @@ require('colors');
 const fs = require('fs-extra');
 const path = require('path');
 
-const env = process.env.ENV || 'dev';
-const src = path.resolve(__dirname, `../env/project_${env}.js`);
+const env = process.env.NODE_ENV || 'dev';
+const src = path.resolve(__dirname, '../env/project.js');
 const target = path.resolve(__dirname, '../common/project.js');
 
 console.log(`Using project_${env}.js`.green);
